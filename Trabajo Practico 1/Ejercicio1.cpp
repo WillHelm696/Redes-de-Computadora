8582c01f44b2d5440a149087b0d97fdda23a8bc6
W.M.A <wmiranda696@gmail.com>
2024-04-08 14:57:42 -0300

    Tp1.v2

--- Trabajo Practico 1/Ejercicio1.cpp
+++ Trabajo Practico 1/Ejercicio1.cpp
@@ -18,19 +18,16 @@ void analizar_tramas(string tramas){
         bool escape = false;
         int suma = 0 ;
         string byte_bandera = tramas.substr(i,2);
-
         if (byte_bandera == "7E"){
-            string trama ;
-            string byte_longitud = tramas.substr(i+2,4) ;
+            string sub_trama ;
+            string byte_longitud = tramas.substr(i+2,4);
             int longitud = stoi(byte_longitud, nullptr, 16);
-
             int j = i+6;
             while ( j < ( i + 6 + longitud * 2 ) ){
                 string byte = tramas.substr(j,2);
                 if (byte == "7E"){
                     string byte_escape = tramas.substr(j-2,2);
                     if (byte_escape != "7D"){
-                        cout << "Escape  " << byte_escape << byte << endl;
                         escape = true;
                         i += 2;
                         break;
@@ -40,48 +37,52 @@ void analizar_tramas(string tramas){
                     flag = true ;
                     suma -= 0x7D ;
                 }
-                trama = trama + byte;
+                sub_trama = sub_trama + byte;
                 suma += stoi(byte, nullptr, 16);            
                 j += 2;
             }
+
             string byte_checksum = tramas.substr(i + longitud * 2 + 6,2);
             int checksum = stoi(byte_checksum, nullptr, 16);
             tramas_totales ++ ;
+
             if (byte_checksum =="7E"){
                     string byte_escape = tramas.substr(j-2,2);
                     if (byte_escape != "7D"){
                         escape = true;
+                        tramas_secuencia_escape ++ ;
                         i-=2;
                     }
             }
-            if (tramas.substr(i+ 8 + trama.length() ,2)!="7E" or escape == true){
+            if (tramas.substr(i+ 8 + sub_trama.length() ,2)!="7E" or sub_trama.length() != longitud*2 ) {
+                byte_checksum = "";
                 tramas_longitud_incorrecta ++ ;
-                cout << byte_bandera << byte_longitud << trama << " Trama: Longitud Incorrecto "<<endl;
+                cout << byte_bandera << byte_longitud << sub_trama << " Trama: Longitud Incorrecto "<<endl;
                 if (escape != true){
                     i +=2 ;
                 }
-            } else {
+            }  
+            else {
                 tramas_longitud_correcta++ ;
                 suma &= 0xFF ;
                 suma = 0xFF - suma;
-                if (flag == true ){
-                    cout << byte_bandera << byte_longitud << trama << byte_checksum << " Trama: Secuencia con escape " << endl;
-                }
                 if (checksum == suma){
                     tramas_checksum_correcto ++ ;
                     //cout << byte_bandera << byte_longitud << trama << byte_checksum << " Trama: Cheksum Correcto " << endl;
                 } else {
                     tramas_checksum_incorrecto ++;
-                    cout << byte_bandera << byte_longitud << trama << byte_checksum << " Trama: Cheksum Incorrecto "<< endl ;
+                    cout << byte_bandera << byte_longitud << sub_trama << byte_checksum << " Trama: Cheksum Incorrecto "<< endl ;
+                }
+                if (flag == true ){
+                    cout << byte_bandera << byte_longitud << sub_trama << byte_checksum << " Trama: Secuencia con escape " << endl;
                 }
             }   
-            i += 8 + trama.length() ;
+            i += 6 + sub_trama.length() + byte_checksum.length();
         } else {
-            cout <<"/ byte SALTADO:" << tramas.substr(i,2)<< endl;
+            cout <<"/ byte SALTADO:" << tramas.substr(i-2,2)<< tramas.substr(i,2)<< tramas.substr(i,2)<< endl;
             i += 2 ;
         }
     }
-
     cout << "Número total de tramas recibidas: " << tramas_totales << endl;
     cout << "Número de tramas con longitud correcta: " << tramas_longitud_correcta << endl;
     cout << "Número de tramas con longitud incorrecta: " << tramas_longitud_incorrecta << endl;
