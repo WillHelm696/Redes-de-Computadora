bfa7f993cd4798dff1f1d1038f9256b3c950435e
W.M.A <wmiranda696@gmail.com>
2024-04-09 15:44:08 -0300

    Tp1.v3

--- Trabajo Practico 1/Ejercicio1.cpp
+++ Trabajo Practico 1/Ejercicio1.cpp
@@ -14,55 +14,60 @@ void analizar_tramas(string tramas){
     int i = 0 ;
 
     while (i <= tramas.length()){
-        bool flag = false; 
-        bool escape = false;
-        int suma = 0 ;
+        bool flag = false;  // Flag verificara si se encontro una secuencia de escape
+        bool escape = false; //escape Verificara si la longitud esta incorecta
+        int suma = 0 ; // Suma calculara el checksun
+
         string byte_bandera = tramas.substr(i,2);
         if (byte_bandera == "7E"){
-            string sub_trama ;
             string byte_longitud = tramas.substr(i+2,4);
+            string sub_trama ; // Sub_Trama Selecsiona se encadena las tramas despues de la longitud
+
             int longitud = stoi(byte_longitud, nullptr, 16);
-            int j = i+6;
+            int j = i+6; // Inicia en la trama despues de calcular la longitud  
             while ( j < ( i + 6 + longitud * 2 ) ){
-                string byte = tramas.substr(j,2);
+                string byte = tramas.substr(j,2); // Extrae byt a byt de la trama
                 if (byte == "7E"){
-                    string byte_escape = tramas.substr(j-2,2);
-                    if (byte_escape != "7D"){
-                        escape = true;
-                        i += 2;
+                    string byte_escape = tramas.substr(j-2,2); // SI se envontro un Byte 7E verifica si antes hay un 7D
+                    if (byte_escape != "7D"){ 
+                        escape = true; // EN caso de no haber un 7D se activa el escape y se toma como longitud incorecto
+                        //i += 2;
                         break;
                     }
-                    tramas_secuencia_escape ++ ;
-                    longitud ++;
-                    flag = true ;
+                    tramas_secuencia_escape ++ ; // En caso de encontrar un 7D se suma a la secuencia de escape
+                    longitud ++; // Se aumenta la longitud en 1 por que 7D no esta es parte de la trama
+                    flag = true ; // Se activa cuando se encuentra una secuencia de escape
                     suma -= 0x7D ;
                 }
-                sub_trama = sub_trama + byte;
-                suma += stoi(byte, nullptr, 16);            
-                j += 2;
+                sub_trama = sub_trama + byte; // Se encadena el byte a las subtramas 
+                suma += stoi(byte, nullptr, 16); // Se lleva a cabo una suma de el byte para verificar el checksum pasando de caracter a base 16
+                j += 2; // j es el contador de la trama hasta la longitud
             }
 
-            string byte_checksum = tramas.substr(i + longitud * 2 + 6,2);
-            int checksum = stoi(byte_checksum, nullptr, 16);
-            tramas_totales ++ ;
+            string byte_checksum = tramas.substr(i + longitud * 2 + 6,2); // Un ves terminadado de recorer la trama se extrae el checksum
+            int checksum = stoi(byte_checksum, nullptr, 16); // Convierte el checksum pasando de caracter a base 16
+            tramas_totales ++ ; // Se aumenta la cantidad de tramas que se encontro 
 
-            if (byte_checksum =="7E"){
-                    string byte_escape = tramas.substr(j-2,2);
-                    if (byte_escape != "7D"){
-                        escape = true;
-                        tramas_secuencia_escape ++ ;
-                        i-=2;
-                    }
-            }
             if (tramas.substr(i+ 8 + sub_trama.length() ,2)!="7E" or sub_trama.length() != longitud*2 ) {
-                byte_checksum = "";
-                tramas_longitud_incorrecta ++ ;
-                cout << byte_bandera << byte_longitud << sub_trama << " Trama: Longitud Incorrecto "<<endl;
+                /* Verifica si la longitud de la trama no coincide 
+                SI tramas.substr(i+ 8 + sub_trama.length() ,2)!="7E"  da falso es mas largo la trama 
+                SI sub_trama.length() != longitud*2 da falso es mas corto la trama
+                */ 
+                byte_checksum = ""; // Si no es la longitud correcta el checksum estara mal por lo que eatara vacio
+                tramas_longitud_incorrecta ++ ; // Se lleva a cabo un acuenta de longitud incorectas
+                cout << byte_bandera << byte_longitud << sub_trama << " Trama: Longitud Incorrecto "<<endl; 
                 if (escape != true){
-                    i +=2 ;
+                    i +=2 ; // 
                 }
-            }  
-            else {
+                if (byte_checksum =="7E"){
+                        string byte_escape = tramas.substr(j-2,2);
+                        if (byte_escape != "7D"){
+                            escape = true;
+                            tramas_secuencia_escape ++ ;
+                            i-=2;
+                        }
+                }
+            } else {
                 tramas_longitud_correcta++ ;
                 suma &= 0xFF ;
                 suma = 0xFF - suma;
@@ -79,10 +84,11 @@ void analizar_tramas(string tramas){
             }   
             i += 6 + sub_trama.length() + byte_checksum.length();
         } else {
-            cout <<"/ byte SALTADO:" << tramas.substr(i-2,2)<< tramas.substr(i,2)<< tramas.substr(i,2)<< endl;
+            cout <<"/ byte SALTADO:" << tramas.substr(i-2,2)<< endl;
             i += 2 ;
         }
     }
+
     cout << "Número total de tramas recibidas: " << tramas_totales << endl;
     cout << "Número de tramas con longitud correcta: " << tramas_longitud_correcta << endl;
     cout << "Número de tramas con longitud incorrecta: " << tramas_longitud_incorrecta << endl;
